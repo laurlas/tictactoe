36cec3d63fd988b4697b4c4886f3097687d64fd7
laurlas <laurentiulas95@gmail.com>
2018-01-07 15:47:47 +0200

    Created basic classes. Handled players assigning to the game

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-$(document).ready(function() {
+$(document).ready(function () {
     let ws = new WebSocket('ws://localhost:8098');
-
-    ws.onmessage = function (response) {
-        if (response.data) {
-            console.log(response.data);
+    $("#new").click(function () {
+        if($("#name").val()==""){
+            alert("Please enter your name");
         }
-    };
-});
+        else{
+            ws.send(JSON.stringify({"action":"newGame","name":$("#name").val()}));
+            $(".join-game").addClass('hidden');
+            $("#loader").removeClass('hidden');
 
+        }
+    });
+    $("#existing").click(function () {
+        if($("#name").val()==""){
+            alert("Please enter your name");
+        }
+        else{
+            ws.send(JSON.stringify({"action":"existingGame","name":$("#name").val()}));
+            $(".join-game").addClass('hidden');
+            $("#loader").removeClass('hidden');
+        }
+    });
+
+
+    let id = 'loader', fill = '#1f1f1f',
+        size = 20, radius = 3, duration = 1000,
+        maxOpacity = 0.6, minOpacity = 0.15;
+    $('<svg id="'+id+'" class="hidden" width="'+(size*3.5)+'" height="'+size+'">' +
+        '<rect width="'+size+'" height="'+size+'" x="0" y="0" rx="'+radius+'" ry="'+radius+'" fill="'+fill+'" fill-opacity="'+maxOpacity+'">' +
+        '<animate attributeName="opacity" values="1;'+minOpacity+';1" dur="'+duration+'ms" repeatCount="indefinite"/>' +
+        '</rect>' +
+        '<rect width="'+size+'" height="'+size+'" x="'+(size*1.25)+'" y="0" rx="'+radius+'" ry="'+radius+'" fill="'+fill+'" fill-opacity="'+maxOpacity+'">' +
+        '<animate attributeName="opacity" values="1;'+minOpacity+';1" dur="'+duration+'ms" begin="'+(duration/4)+'ms" repeatCount="indefinite"/>' +
+        '</rect>' +
+        '<rect width="'+size+'" height="'+size+'" x="'+(size*2.5)+'" y="0" rx="'+radius+'" ry="'+radius+'" fill="'+fill+'" fill-opacity="'+maxOpacity+'">' +
+        '<animate attributeName="opacity" values="1;'+minOpacity+';1" dur="'+duration+'ms" begin="'+(duration/2)+'ms" repeatCount="indefinite"/>' +
+        '</rect>' +
+        '</svg>').appendTo('body');
+});
--- server.js
+++ server.js
@@ -5,29 +5,105 @@ const app = express();
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
-let clients = [];
+let players = [];
+let games = [];
+
 let count=0;
+let gameCount=0;
 
 wss.on('connection', function connection(ws) {
-    clients.push(ws);
+    ws.player='none';
+    ws.partner='none';
     let id = count++;
-    for (client in clients) {
-        try {
-            clients[client].send(JSON.stringify("hey"));
-        } catch (e) {
+    players[id]=new Player(id, ws);
 
-        }
-    }
     ws.on('message', function incoming(message) {
-
+        msg=JSON.parse(message);
+        if(msg.action==='newGame'){
+            players[id].name=msg.name;
+            let pair = assignplayers();
+            if(pair) {
+                let gameId = count++;
+                gameCount++;
+                games[gameId]= new Game(gameCount, players[pair.x], players[pair.o]);
+                games[gameId].start();
+            }
+        }
     });
     ws.on('close', function close(ws) {
-        delete clients[id];
+        delete players[id];
         console.log('close');
     });
 });
 
-
 server.listen(8098, function listening() {
     console.log('Listening on %d', server.address().port);
 });
+
+function assignplayers(){
+    let count=0;
+    let x={};
+    let o={};
+    for (player in players) {
+        if(players[player].assigned===false && players[player].name!==""){
+            if(count===0){
+                x=player;
+                count++;
+            }
+            else if(count===1){
+                o=player;
+                count++;
+                break;
+            }
+        }
+    }
+    if(count===2){
+        return {'x':x,'o':o};
+    }
+    else{
+        return false;
+    }
+}
+class Game{
+    constructor(id,x,o) {
+        this.id= id;
+        this.board= new Board();
+        this.x= x;
+        this.o= o;
+        this.x.assigned=true;
+        this.o.assigned=true;
+        console.log(players);
+    }
+    start(){
+        this.x.send(JSON.stringify({"action":"start","player":"x","game":this.id}));
+        this.o.send(JSON.stringify({"action":"start","player":"o","game":this.id}));
+    }
+}
+class Player{
+    constructor(id,ws , name) {
+        this.id=id;
+        this.ws=ws;
+        this.assigned=false;
+        if(typeof name !== 'undefined'){
+            this.name = name;
+        }
+        else {
+            this.name = "";
+        }
+    }
+}
+class Board {
+    constructor(lines) {
+        if(typeof lines !== 'undefined'){
+            this.lines = lines;
+        }
+        else {
+            this.lines = [
+                "", "", "",
+                "", "", "",
+                "", "", ""
+            ];
+        }
+    }
+
+}
